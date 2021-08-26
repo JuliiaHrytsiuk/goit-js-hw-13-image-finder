b5c1a8df3bf7250bb03449ec8d3d17233ac6876b
JuliiaHrytsiuk <78194183+JuliiaHrytsiuk@users.noreply.github.com>
2021-08-26 18:21:06 +0300

    semicolon

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import './sass/main.scss';
+// import PixabayApiService from './js/apiService';
 
 const refs = {
   searchForm: document.querySelector('#search-form'),
   galleryContainer: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('[data-action="my-element-selector"]'),
 };
 
 refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+// var searchQuery = '';
 
 function onSearch(event) {
   event.preventDefault();
 
   const searchQuery = event.currentTarget.elements.query.value;
 
+  const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=1&per_page=12&key=23095971-ceaeacbf51a21ad754e50720c`;
+
+  fetch(url)
+    .then(r => r.json())
+    .then(console.log);
+}
+
+function onLoadMore() {
   const url =
     'https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=1&per_page=12&key=23095971-ceaeacbf51a21ad754e50720c';
 
@@ -19,3 +32,9 @@ function onSearch(event) {
     .then(r => r.json())
     .then(console.log);
 }
+
+// const element = document.getElementById('.my-element-selector');
+// element.scrollIntoView({
+//   behavior: 'smooth',
+//   block: 'end',
+// });
