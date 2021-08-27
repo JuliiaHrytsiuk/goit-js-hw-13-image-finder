4fa4d72274f5713c77d7720bb09afd6ebb894d81
JuliiaHrytsiuk <78194183+JuliiaHrytsiuk@users.noreply.github.com>
2021-08-28 01:55:13 +0300

    updated

--- src/js/components/notify.js
+++ src/js/components/notify.js
@@ -1,7 +1,5 @@
 import { error, defaultModules } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
-import * as PNotifyMobile from '@pnotify/mobile';
-import '@pnotify/mobile/dist/PNotifyMobile.css';
 import '@pnotify/core/dist/BrightTheme.css';
 
 export default error;
